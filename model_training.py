ec0109fa7c488b4c462bfd3ad67de04714dc9dc6
KarthikeyaAndhoju <104316957+KarthikeyaAndhoju@users.noreply.github.com>
2024-08-13 15:21:03 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -2,8 +2,9 @@ from data_preprocessing import load_and_preprocess_data
 from sklearn.linear_model import LinearRegression
 
 def train_model(filepath):
+    # Load and preprocess the data
     X_train, X_test, y_train, y_test = load_and_preprocess_data(filepath)
-    model = LinearRegression()
+
 
 if __name__ == "__main__":
     filepath = "simple_dataset.csv"  
